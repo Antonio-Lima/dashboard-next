b7a9d4447d929bf2fa00ff2e44c18da9d792318b
Antonio-Lima <gutto1998@gmail.com>
2024-06-05 19:26:33 -0300

    Instalação da fonte.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
+
+import "@fontsource/public-sans";
+import "@fontsource/public-sans/100.css";
+import "@fontsource/public-sans/200.css";
+import "@fontsource/public-sans/300.css";
+import "@fontsource/public-sans/400.css";
+import "@fontsource/public-sans/500.css";
+import "@fontsource/public-sans/600.css";
+import "@fontsource/public-sans/700.css";
+import "@fontsource/public-sans/800.css";
+import "@fontsource/public-sans/900.css";
+
 import "./globals.css";
+
 import Header from "@/components/Header";
 
 const font = Public_Sans({ subsets: ["latin"] });
