64af022fbb93434d72f46402bbf510ce8309856d
Antonio-Lima <gutto1998@gmail.com>
2024-06-04 22:16:57 -0300

    Implementado Summary.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Summary from "@/components/Summary";
+import { Box } from "@mui/material";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <Box>
+      <Summary />
+    </Box>
+  );
 }
