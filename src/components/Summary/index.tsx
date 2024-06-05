88254006cde95923197a427e237702a2aa7e4e2c
Antonio-Lima <gutto1998@gmail.com>
2024-06-04 22:44:42 -0300

    Iniciado o desenvolvimento de cards para summary.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,10 +4,9 @@ import { Box, Card, Typography } from "@mui/material";
 import Image from "next/image";
 import Button from "../Button";
 import { Add } from "@mui/icons-material";
+import { ReactNode } from "react";
 
 export default function Summary() {
-  const now = new Date();
-
   return (
     <Box
       width="100%"
@@ -17,7 +16,7 @@ export default function Summary() {
       display="flex"
       alignItems="center"
     >
-      <Box color={THEME.colors.white} maxWidth="731px" mx="auto">
+      <Box color={THEME.colors.white} maxWidth="731px" width="100%" mx="auto">
         <Typography
           fontWeight="regular"
           fontSize="0.625rem"
@@ -33,6 +32,42 @@ export default function Summary() {
         >
           {getFormatedActualDate()}
         </Typography>
+        <Box width="100%" display="flex" gap="1.25rem" mt="1rem">
+          <SummaryCard
+            value="0"
+            goal="5"
+            title={
+              <>
+                PESQUISAS <br /> EM CAMPO
+              </>
+            }
+            variant="points"
+          />
+          <SummaryCard
+            value="300"
+            goal=""
+            title={
+              <>
+                PESQUISAS EM
+                <br />
+                ROTEIRIZAÇÃO
+              </>
+            }
+            variant="none"
+          />
+          <SummaryCard
+            value="2.000"
+            goal="10.000"
+            title={
+              <>
+                DISPAROS
+                <br />
+                FEITOS
+              </>
+            }
+            variant="progression"
+          />
+        </Box>
       </Box>
       <Card>
         <Box
@@ -61,6 +96,66 @@ export default function Summary() {
           <Button label="Nova Pesquisa" icon={<Add />}></Button>
         </Box>
       </Card>
+    </Box>
+  );
+}
+
+interface CardProps {
+  value: string;
+  goal: string;
+  title: ReactNode;
+  variant: "points" | "progression" | "none";
+}
+
+function SummaryCard({ value, goal, title, variant }: CardProps) {
+  return (
+    <Box
+      width="100%"
+      borderRadius="4px"
+      bgcolor={THEME.colors["gray-500"]}
+      p="1.375rem"
+      border="1px solid"
+      borderColor={THEME.colors["gray-500"]}
+    >
+      <Goal value={value} goal={goal} />
+      <Typography
+        fontSize="0.875rem"
+        fontWeight="medium"
+        color={THEME.colors["gray-300"]}
+        mt="0.375rem"
+      >
+        {title}
+      </Typography>
+    </Box>
+  );
+}
+
+interface GoalProps {
+  goal: string;
+  value: string;
+}
+function Goal({ goal, value }: GoalProps) {
+  return (
+    <Box display="flex" alignItems="flex-end">
+      <Typography
+        fontWeight="bold"
+        fontSize="1.875rem"
+        lineHeight="1.875rem"
+        color={THEME.colors.white}
+      >
+        {value}
+      </Typography>
+      {goal && (
+        <Typography
+          fontWeight="regular"
+          fontSize="1.125rem"
+          lineHeight="1.125rem"
+          color={THEME.colors["gray-300"]}
+          mb="2px"
+        >
+          /{goal}
+        </Typography>
+      )}
     </Box>
   );
 }
