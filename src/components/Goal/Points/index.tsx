fc3c7d00c35d97ac1cfef4610a5d6894df137f34
Antonio-Lima <gutto1998@gmail.com>
2024-06-05 13:55:12 -0300

    Ajuste no layout.

--- src/components/Goal/Points/index.tsx
+++ src/components/Goal/Points/index.tsx
@@ -9,14 +9,14 @@ interface GoalProps {
 export default function GoalPoints({ value, goal }: GoalProps) {
   const array = Array.from({ length: goal }, (_, i) => i + 1);
   return (
-    <Box mt="2.625rem" display="flex" gap="0.375rem">
+    <Box display="flex" gap="0.375rem">
       {array.map((item) => (
         <Box
           key={item}
           width={12}
           height={12}
           border="1px solid"
-          borderColor={THEME.colors["gray-300"]}
+          borderColor={THEME.colors.white}
           borderRadius={999}
           bgcolor={item <= value ? THEME.colors.white : "transparent"}
         />
