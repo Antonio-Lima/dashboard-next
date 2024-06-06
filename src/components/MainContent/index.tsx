2b1c3aa6b94696ce3cb0959a0b645f2bf10fc608
Antonio-Lima <gutto1998@gmail.com>
2024-06-06 20:54:19 -0300

    Correção no nome da constante.

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -23,17 +23,17 @@ export default function MainContent({
   credits,
   gridArea,
 }: MainContentProps) {
-  const large = useMediaQuery(THEME.mediaQuery.xlarge);
+  const xlarge = useMediaQuery(THEME.mediaQuery.xlarge);
 
   return (
     <Box
       width="100%"
       gridArea={gridArea}
       py="55px"
-      px={large ? 0 : "80px"}
-      pr={large ? "367px" : "80px"}
+      px={xlarge ? 0 : "80px"}
+      pr={xlarge ? "367px" : "80px"}
     >
-      <Box maxWidth={large ? "731px" : "480px"} width="100%" mx="auto">
+      <Box maxWidth={xlarge ? "731px" : "480px"} width="100%" mx="auto">
         <Typography
           color={THEME.colors.black}
           fontWeight="bold"
@@ -51,13 +51,13 @@ export default function MainContent({
           width="100%"
           gap="20px"
           pt="52px"
-          flexDirection={large ? "row" : "column"}
+          flexDirection={xlarge ? "row" : "column"}
         >
           <Credits credits={credits} />
           <Box
             display="flex"
             width="100%"
-            maxWidth={large ? "343px" : "none"}
+            maxWidth={xlarge ? "343px" : "none"}
             flexDirection="column"
             gap="20px"
           >
