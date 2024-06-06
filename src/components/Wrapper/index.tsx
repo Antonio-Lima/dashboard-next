6d356994188bfe19425834cca318f0f9a1ac3104
Antonio-Lima <gutto1998@gmail.com>
2024-06-06 20:18:08 -0300

    Criado wrapper para controle da tela.

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { THEME } from "@/constants/theme";
+import { Box, useMediaQuery } from "@mui/material";
+import { ReactNode } from "react";
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+export default function Wrapper({ children }: WrapperProps) {
+  const large = useMediaQuery(THEME.mediaQuery.large);
+
+  return (
+    <Box
+      display="grid"
+      sx={{
+        gridTemplateColumns: "1fr 1fr minmax(0, 384px)",
+        gridTemplateAreas: `"summary summary ${
+          large ? "notifications" : "summary"
+        }"
+      "main main notifications"`,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
